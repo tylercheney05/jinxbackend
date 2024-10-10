24bfa523dd62706389eafe4e13dc3c809df98c52
Tyler Cheney <tyler.cheney.05@gmail.com>
2024-10-09 22:50:32 -0700

    Add sodas to seed script

--- scripts/seed_data.py
+++ scripts/seed_data.py
@@ -1,4 +1,5 @@
 from cups.models import Cup
+from sodas.models import Soda
 
 
 def run():
@@ -7,3 +8,17 @@ def run():
     print("16 oz cup created")
     Cup.objects.get_or_create(size="32", price=3.5, conversion_factor=2)
     print("32 oz cup created")
+
+    print("\n")
+    print("--------------------")
+    print("\n")
+
+    # Add sodas
+    Soda.objects.get_or_create(name="Coke")
+    print("Coke created")
+    Soda.objects.get_or_create(name="Sprite")
+    print("Sprite created")
+    Soda.objects.get_or_create(name="Dr. Pepper")
+    print("Dr. Pepper created")
+    Soda.objects.get_or_create(name="Mtn. Dew")
+    print("Mtn. Dew created")
