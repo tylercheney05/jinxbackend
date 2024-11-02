4bfc0f2fb49f5435612671e22d07b1ebdae88a5b
Tyler Cheney <tyler.cheney.05@gmail.com>
2024-11-02 08:29:00 -0700

    update scripts

--- scripts/seed_data.py
+++ scripts/seed_data.py
@@ -1,11 +1,5 @@
-import os
-
-import pandas as pd
-import pytz
-
 from cups.models import Cup
 from flavors.models import Flavor, FlavorGroup
-from jinxbackend.settings import BASE_DIR
 from locations.models import Location
 from menuitems.models import (
     LimitedTimeMenuItem,
@@ -14,23 +8,13 @@ from menuitems.models import (
     MenuItemFlavor,
 )
 from orders.models import (
-    CustomOrder,
-    CustomOrderFlavor,
-    CustomOrderFlavorCustomOrder,
     Discount,
     DiscountCupSize,
     DiscountPercentOff,
     DiscountPrice,
-    Order,
-    OrderDiscount,
-    OrderItem,
-    OrderItemCustomOrder,
-    OrderItemMenuItem,
     OrderName,
-    OrderPaidAmount,
 )
 from sodas.models import Soda
-from users.models import User
 
 
 def run():
@@ -461,7 +445,7 @@ def run():
     print("Aminé created")
     OrderName.objects.get_or_create(name="Busta Rhymes")
     print("Busta Rhymes created")
-    OrderName.objects.get_or_create(name="Kanye West The Boy")
+    OrderName.objects.get_or_create(name="Kanye West")
     print("Kanye West created")
     OrderName.objects.get_or_create(name="Ashanti")
     print("Ashanti created")
@@ -497,7 +481,7 @@ def run():
     print("Chance The Rapper created")
     OrderName.objects.get_or_create(name="Breezy")
     print("Breezy created")
-    OrderName.objects.get_or_create(name="Ms. Lauryn Hill")
+    OrderName.objects.get_or_create(name="Lauryn Hill")
     print("Ms. Lauryn Hill created")
     OrderName.objects.get_or_create(name="BadGal RiRi")
     print("BadGal RiRi created")
@@ -507,7 +491,7 @@ def run():
     print("Future created")
     OrderName.objects.get_or_create(name="Jay-Z")
     print("Jay-Z created")
-    OrderName.objects.get_or_create(name="BIg Poppa")
+    OrderName.objects.get_or_create(name="Big Poppa")
     print("Big Poppa created")
     OrderName.objects.get_or_create(name="Slim Shady")
     print("Slim Shady created")
@@ -523,6 +507,37 @@ def run():
     print("Bow Wow created")
     OrderName.objects.get_or_create(name="Ciara")
     print("Ciara created")
+    OrderName.objects.get_or_create(name="Biggie Smalls")
+    print("Biggie Smalls created")
+    OrderName.objects.get_or_create(name="Alicia Keys")
+    print("Alicia Keys created")
+    OrderName.objects.get_or_create(name="Mariah Carey")
+    print("Mariah Carey created")
+    OrderName.objects.get_or_create(name="Jamie Foxx")
+    print("Jamie Foxx created")
+    OrderName.objects.get_or_create(name="Destiny's Child")
+    print("Destiny's Child created")
+    OrderName.objects.get_or_create(name="Logic")
+    print("Logic created")
+    OrderName.objects.get_or_create(name="DJ Khaled")
+    print("DJ Khaled created")
+    OrderName.objects.get_or_create(name="Ne-Yo")
+    print("Ne-Yo created")
+    OrderName.objects.get_or_create(name="T-Pain")
+    print("T-Pain created")
+    OrderName.objects.get_or_create(name="Milli Vanilli")
+    print("Milli Vanilli created")
+    OrderName.objects.get_or_create(name="Lil Wayne")
+    print("Lil Wayne created")
+    OrderName.objects.get_or_create(name="Kali Uchis")
+    print("Kali Uchis created")
+    OrderName.objects.get_or_create(name="J. Cole")
+    print("J. Cole created")
+    OrderName.objects.get_or_create(name="The Weeknd")
+    print("The Weeknd created")
+    OrderName.objects.get_or_create(name="Post Malone")
+    print("Post Malone created")
+    
 
     print("\n")
     print("--------------------")
@@ -539,187 +554,3 @@ def run():
     DiscountPrice.objects.get_or_create(discount=lime_bois_am_perk, price=4)
     DiscountCupSize.objects.get_or_create(discount=lime_bois_am_perk, cup=_16_oz)
     print("Lime Boi's AM Perk discount created")
-
-    print("\n")
-    print("--------------------")
-    print("\n")
-
-    file_location = os.path.join(BASE_DIR, "files", "Order Tracking.xlsx")
-    menu_item_orders = pd.read_excel(file_location, sheet_name="Menu Item Orders")
-    custom_orders = pd.read_excel(file_location, sheet_name="BYO Orders")
-
-    cup_mapping = {
-        "16 oz": "16",
-        "32 oz": "32",
-    }
-
-    ## Add Menu Item Orders
-    prev_order_num = 0
-    for label, row in menu_item_orders.iterrows():
-        order_num = row["Order #"]
-        date = row["Date"]
-        location = row["Location"]
-        cup = row["Cup"]
-        zero_sugar = row["Zero Sugar"]
-        menu_item = row["Menu Item"]
-        charged = row["Charged"]
-        discount_code = row["Discount Code"]
-
-        location_obj = Location.objects.get(name=location)
-        cup_obj = Cup.objects.get(size=cup_mapping[cup])
-        menu_item_obj = MenuItem.objects.get(name__iexact=menu_item)
-
-        if order_num != prev_order_num:
-            total_charged = charged
-            date = date.to_pydatetime()
-            pacific = pytz.timezone("US/Pacific")
-            utc = pytz.utc
-            date = pacific.localize(date).astimezone(utc)
-
-            order = Order.objects.create(
-                date=date,
-                collected_by=User.objects.first(),
-                is_paid=True,
-                location=location_obj,
-                is_complete=True,
-                is_in_progress=False,
-            )
-            print(f"Order {order_num} created")
-
-            OrderPaidAmount.objects.create(order=order, paid_amount=total_charged)
-            print(f"OrderPaidAmount {order_num} ${total_charged} created")
-        else:
-            total_charged += charged
-            OrderPaidAmount.objects.filter(order=order).update(
-                paid_amount=total_charged
-            )
-            print(f"OrderPaidAmount {order_num} ${total_charged} updated")
-
-        order_item = OrderItem.objects.create(
-            order=order, cup=cup_obj, low_sugar=zero_sugar, is_prepared=True
-        )
-        print(f"OrderItem {order_num} {menu_item} created")
-
-        OrderItemMenuItem.objects.create(order_item=order_item, menu_item=menu_item_obj)
-        print(f"OrderItemMenuItem {order_num} {menu_item} created")
-
-        if pd.notna(discount_code):
-            discount = Discount.objects.get(code=discount_code)
-            OrderDiscount.objects.create(order=order, discount=discount)
-            print(f"OrderDiscount {order_num} {discount_code} created")
-        elif charged == 0:
-            OrderDiscount.objects.create(order=order, discount=free_discount)
-            print(f"OrderDiscount {order_num} FREE created")
-
-        prev_order_num = order_num
-
-    print("\n")
-    print("--------------------")
-    print("\n")
-
-    ## Add Custom Orders
-    prev_order_num = 0
-    for label, row in custom_orders.iterrows():
-        order_num = row["Order #"]
-        date = row["Date"]
-        location = row["Location"]
-        cup = row["Cup"]
-        zero_sugar = row["Zero Sugar"]
-        soda = row["Soda"].strip()
-        flavor1 = row["Flavor 1"]
-        flavor2 = row["Flavor 2"]
-        flavor3 = row["Flavor 3"]
-        flavor4 = row["Flavor 4"]
-        charged = row["Charged"]
-
-        location_obj = Location.objects.get(name=location)
-        cup_obj = Cup.objects.get(size=cup_mapping[cup])
-        soda_obj = Soda.objects.get(name=soda)
-
-        flavor1 = Flavor.objects.get(name=flavor1) if pd.notna(flavor1) else None
-        flavor2 = Flavor.objects.get(name=flavor2) if pd.notna(flavor2) else None
-        flavor3 = Flavor.objects.get(name=flavor3) if pd.notna(flavor3) else None
-        flavor4 = Flavor.objects.get(name=flavor4) if pd.notna(flavor4) else None
-
-        if order_num != prev_order_num:
-            total_charged = charged
-            date = date.to_pydatetime()
-            pacific = pytz.timezone("US/Pacific")
-            utc = pytz.utc
-            date = pacific.localize(date).astimezone(utc)
-
-            order = Order.objects.create(
-                date=date,
-                collected_by=User.objects.first(),
-                is_paid=True,
-                location=location_obj,
-                is_complete=True,
-                is_in_progress=False,
-            )
-            print(f"Order {order_num} created")
-
-            OrderPaidAmount.objects.create(order=order, paid_amount=total_charged)
-            print(f"OrderPaidAmount {order_num} ${total_charged} created")
-        else:
-            total_charged += charged
-            OrderPaidAmount.objects.filter(order=order).update(
-                paid_amount=total_charged
-            )
-            print(f"OrderPaidAmount {order_num} ${total_charged} updated")
-
-        order_item = OrderItem.objects.create(
-            order=order, cup=cup_obj, low_sugar=zero_sugar, is_prepared=True
-        )
-        print(f"OrderItem {order_num} Custom Order created")
-
-        custom_order = CustomOrder.objects.create(
-            soda=soda_obj,
-        )
-        print(f"CustomOrder {order_num} created")
-
-        OrderItemCustomOrder.objects.create(
-            order_item=order_item, custom_order=custom_order
-        )
-        print(f"OrderItemCustomOrder {order_num} created")
-
-        quantity_mapping = {
-            "16 oz": 1,
-            "32 oz": 2,
-        }
-
-        if flavor1:
-            custom_flavor1 = CustomOrderFlavor.objects.create(
-                flavor=flavor1, quantity=quantity_mapping[cup]
-            )
-            CustomOrderFlavorCustomOrder.objects.create(
-                custom_order_flavor=custom_flavor1, custom_order=custom_order
-            )
-            print(f"CustomOrderFlavor {order_num} {flavor1} created")
-        if flavor2:
-            custom_flavor2 = CustomOrderFlavor.objects.create(
-                flavor=flavor2, quantity=quantity_mapping[cup]
-            )
-            CustomOrderFlavorCustomOrder.objects.create(
-                custom_order_flavor=custom_flavor2, custom_order=custom_order
-            )
-            print(f"CustomOrderFlavor {order_num} {flavor2} created")
-        if flavor3:
-            custom_flavor3 = CustomOrderFlavor.objects.create(
-                flavor=flavor3, quantity=quantity_mapping[cup]
-            )
-            CustomOrderFlavorCustomOrder.objects.create(
-                custom_order_flavor=custom_flavor3, custom_order=custom_order
-            )
-            print(f"CustomOrderFlavor {order_num} {flavor3} created")
-        if flavor4:
-            custom_flavor4 = CustomOrderFlavor.objects.create(
-                flavor=flavor4, quantity=quantity_mapping[cup]
-            )
-            CustomOrderFlavorCustomOrder.objects.create(
-                custom_order_flavor=custom_flavor4, custom_order=custom_order
-            )
-            print(f"CustomOrderFlavor {order_num} {flavor4} created")
-
-        if charged == 0:
-            OrderDiscount.objects.create(order=order, discount=free_discount)
-            print(f"OrderDiscount {order_num} FREE created")
