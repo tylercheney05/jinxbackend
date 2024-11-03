445bbbee4fa9f53f2fc76e04e9dd83c98e3a5d4e
Tyler Cheney <tyler.cheney.05@gmail.com>
2024-11-03 10:05:29 -0800

    update channel_layers in settings

--- jinxbackend/settings.py
+++ jinxbackend/settings.py
@@ -177,7 +177,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [(HOST, REDIS_PORT)],
+            "hosts": [os.environ.get("REDIS_URL", "redis://localhost:6379")],
         },
     },
 }
