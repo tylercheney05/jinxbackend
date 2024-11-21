08dccf2dc9e8e636e6a43ec6525127d2589f0ddb
Tyler Cheney <tyler.cheney.05@gmail.com>
2024-11-20 16:52:58 -0800

    add ASGI_THREADS to asgi.py file

--- jinxbackend/asgi.py
+++ jinxbackend/asgi.py
@@ -6,6 +6,10 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jinxbackend.settings")
+
+# Set ASGI_THREADS environment variable
+os.environ.setdefault("ASGI_THREADS", "4")  # Adjust the number of threads as needed
+
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
